f718af62ef4af9f648ea9ce4412386c67f3ad8ea
Jeremy Wright <jeremy@codestrokes.com>
2013-06-18 21:01:18 -0700

    Whoa! 028 works

--- 028.hs
+++ 028.hs
@@ -1,6 +1,8 @@
 
---corner_seq = 1 : (fn 1 (+2))
+corner_seq = 1 : (fn 1 (+2))
     
 
-fn s adder = adder s : (adder . adder) s : (adder . adder . adder) s : (adder. adder . adder . adder) s : fn ((adder . adder . adder . adder ) s) (adder . adder)
+fn s adder = adder s : (adder . adder) s : (adder . adder . adder) s : (adder. adder . adder . adder) s : fn ((adder . adder . adder . adder ) s) (adder . (+2))
+
+main = print $ (sum . takeWhile (<=1001^2)) corner_seq
 
