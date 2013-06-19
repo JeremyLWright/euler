e482d64c24841db65e70f32e6d56cb5fe70ee3e5
Jeremy Wright <jeremy@codestrokes.com>
2013-06-18 21:22:35 -0700

    The problem statement stated 1 is not part of the solution

--- 030.hs
+++ 030.hs
@@ -3,7 +3,7 @@ import Data.Char
 checkfifthSum n = powerSum == n
     where powerSum = sum $ map (^5) $  map digitToInt $ show n
 
-fifthSums = filter checkfifthSum [1..limit]
+fifthSums = filter checkfifthSum [2..limit]
     where limit = (sum . map (9^5*)) [1..5]
 
 main = print $ sum fifthSums
