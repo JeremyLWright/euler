3b15572758be605c1aefbbfa90a7f3c271654ac7
Jeremy Wright <jeremy@codestrokes.com>
2013-06-25 20:48:45 -0700

    Euler Values printing now

--- Makefile
+++ Makefile
@@ -2,19 +2,26 @@ SRCS=$(wildcard src/*.hs)
 LIBS=$(wildcard src/Euler/*.hs)
 PROGS=$(patsubst %.hs,%,$(SRCS))
 COMPILE_TIMES=dist/compile.dat
+HC=ghc
+HC_OPTS=-O2 -isrc/ --make
+
 all: $(PROGS)
 
 %: %.hs $(LIBS)
 	echo -n "$*.hs\t" >> $(COMPILE_TIMES)
-	/usr/bin/time --format=%e ghc -outputdirdist -isrc/ -O2 --make $*.hs 2>> $(COMPILE_TIMES)
+	/usr/bin/time --format=%e $(HC) $(HC_OPTS) $*.hs 2>> $(COMPILE_TIMES)
 
-test: euler.png
+check: euler.png
 
-times.dat: $(PROGS)
-	python EulerValues.py
+dist/times.dat: $(PROGS)
+	util/EulerValues.py --answers util/answers.js --file dist/times.dat $(PROGS)
 
-euler.png: times.dat
+euler.png: dist/times.dat
 	octave process.m
 
 clean: 
 	rm -rf $(PROGS) dist/*
+	find src -name *.o -exec rm -rf {} \;
+	find src -name *.hi -exec rm -rf {} \;
+
+
--- util/EulerValues.py
+++ util/EulerValues.py
@@ -1,49 +1,36 @@
+#!/usr/bin/env python2
+
 import sys, time, subprocess
-problems = (
-        ("001", "233168"),
-        ("002", "4613732"),
-        ("003", "6857"),
-        ("004", "906609"),
-        ("005", "232792560"),
-        ("006", "25164150"),
-        ("007", "104743"),
-        ("008", "40824"),
-        ("009", "31875000"),
-        ("010", "142913828922"),
-        ("011", "70600674"),
-        ("012", "76576500"),
-        ("013", "5537376230"),
-        ("014", "(525,837799)"),
-        ("015", "137846528820"),
-        ("016", "1366"),
-        ("017", "21124"),
-        ("018", "1074"),
-        ("019", "171"),
-        ("020", "648"),
-        ("021", "31626"),
-        ("022", "871198282"),
-        ("023", "4179871"),
-        ("024", "\"2783915460\""),
-        ("025", "4782"),
-        ("026", "983"),
-        ("027", "-59231"),
-        ("028", "669171001"),
-        ("029", "9183"),
-        ("030", "443839"),
-        ("034", "40730"),
-        ("046", "\"working...\""),
-)
+import json
+import argparse
+import pdb
+import os
 
+def main():
+    parser = argparse.ArgumentParser(description='Verify Project Euler Solutions')
+    parser.add_argument('programs', metavar='P', type=str, nargs='*', help='solutions to run')
+    parser.add_argument('--file', required=True, help="file to write times to")
+    parser.add_argument('--answers', required=True, help="json file describing the answers")
+    
+    args = parser.parse_args()
 
+    answers = json.load(open(args.answers, "r"))
+    ftime = open(args.file, "w")
 
-ftime = open("times.dat", "w")
-for p,a in problems:
-    start_time = time.time()
-    proc = subprocess.Popen("./"+p, stdout=subprocess.PIPE)
-    end_time = time.time() - start_time
-    result = proc.stdout.read().strip()
-    if result == a:
-        print "[{0}] Passed {1} seconds".format(p, end_time)
-    else:
-        print "[{0}] Failed Expected: {1} Got: {2}".format(p, result, a)
-    ftime.write("problem_"+p+"\t"+str(end_time)+"\n")
+    for prog in args.programs:
+        p = os.path.basename(prog)
+        wd = os.path.dirname(os.path.abspath(prog))
+        a = answers[p]
+        start_time = time.time()
+        proc = subprocess.Popen("./"+p, cwd=wd, stdout=subprocess.PIPE)
+        result = proc.stdout.read().strip()
+        end_time = time.time() - start_time
+        if result == a:
+            print "[{0}] Passed {1} seconds".format(p, end_time)
+        else:
+            print "[{0}] Failed Expected: {1} Got: {2}".format(p, a, result)
+        ftime.write(p+"\t"+str(end_time)+"\n")
+
+if __name__ == "__main__":
+    main()
+
