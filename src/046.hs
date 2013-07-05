ee72c4f970badfd054759cb5a990b1b2a8e151ef
Jeremy Wright <jeremy@codestrokes.com>
2013-07-04 19:19:35 -0700

    Added a property for testing with QuickCheck. Use ghc -e runTests 046.hs
    to run all the tests.

--- src/046.hs
+++ src/046.hs
@@ -1,3 +1,5 @@
+import Test.HUnit
+import Test.QuickCheck
 
 divide2 :: Integral a => a -> a
 divide2 x = x `div` 2
@@ -7,3 +9,14 @@ square_term x p | (divide2 x) `mod` p == 0 = True -- is can x be a square term o
                 where second_term = divide2 x 
 
 main = print "working..."
+ 
+-- Tests --
+{-# LANGUAGE TemplateHaskell #-} 
+prop_div2:: Int -> Bool
+prop_div2 n = (divide2 n) == (n `div` 2)
+
+--tests = TestList $ map TestCase
+--    [assertEqual (divide2 2) 1]
+runTests = do
+    quickCheck prop_div2
+    --runTestTT tests
