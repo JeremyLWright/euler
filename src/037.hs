902c2886bf6de54a8b35fd39d6a9d821000afce7
Jeremy Wright <jeremy@codestrokes.com>
2014-03-14 14:54:11 -0700

    Big speed boost from forum hints.

--- src/037.hs
+++ src/037.hs
@@ -1,16 +1,20 @@
-import Euler.NumberTheory
-import Data.Digits
+---Using the fact that only prime digits can appear on the leftmost or rightmost--and only odd digits (minus 5) can appear in the middle.
 
-reverseNum =  unDigits 10 . digitsRev 10
+import Data.Numbers.Primes (isPrime)
+import Control.Monad (liftM2)
+import Data.List (inits)
 
-leftToRightPrime 0 = True
-leftToRightPrime x  | isPrime x = leftToRightPrime $ x `div` 10
-                    | otherwise = False
+primeDigits= "2357"
 
-rightToLeftPrime x = leftToRightPrime $ reverseNum x
+fillerDigits = "1379"
 
---2,3,5,7 are dropped from the result
---prob037 = take 11 $ drop 4 $ filter rightToLeftPrime $ filter leftToRightPrime primes
-prob037 = take 11 $ drop 4 $ [x | x <- primes, leftToRightPrime x, rightToLeftPrime x]
+findTruncatablePrimes = map read $ recur [] [""]
+  where recur acc cur = let cur' = filter (isPrime . read) $ liftM2 (:) fillerDigits cur
+                            new  = filter (all (isPrime . read) . tail . inits) $ liftM2 (:) primeDigits cur
+                        in if null cur'
+                           then acc
+                           else recur (acc ++ new) cur'
+
+prob037 = sum $ drop  (length primeDigits) findTruncatablePrimes
 
 main = print prob037
