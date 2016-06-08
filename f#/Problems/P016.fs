27c912d051accf548104d9c2e1d1f21cc8929212
Jeremy Wright <jeremy.wright@commscope.com>
2016-06-07 22:24:43 -0700

    Problem 16

---
+++ f#/Problems/P016.fs
@@ -0,0 +1,22 @@
+﻿namespace ProjectEuler
+
+module ``Problem 16`` =
+    open Prime
+    open NUnit.Framework
+    open FsUnit
+    open System
+         
+    let expSum n = 
+         (pown 2I n) 
+         |> string 
+         |> Seq.map (fun s -> int(s) - int('0'))
+         |> Seq.sum
+    [<Test>]
+    [<Category("example")>]
+    let example () =
+         expSum 15 |> should equal 26
+
+    [<Test>]
+    [<Category("Solution")>]
+    let solution () =
+         expSum 1000 |> should equal 1366
