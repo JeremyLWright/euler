97f73a4ff75a711b5acd388a65451f2b29278484
Jeremy Wright <jeremy.wright@commscope.com>
2016-07-07 22:23:32 -0700

    Adding prime Sieve.

--- f#/Problems/P005.fs
+++ f#/Problems/P005.fs
@@ -4,6 +4,7 @@ module ``Problem 5`` =
     open NUnit.Framework
     open FsUnit
     open Euclid
+    open MathNet.Numerics
 
     //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
     //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
@@ -11,9 +12,9 @@ module ``Problem 5`` =
     [<Test>]
     [<Category("example")>]
     let example () =
-        Seq.reduce Euclid.lcm [1..10] |> should equal 2520
+        Euclid.LeastCommonMultiple [|1L..10L|]  |> should equal 2520
 
     [<Test>]
     [<Category("solution")>]
     let solution () =
-        Seq.reduce Euclid.lcm [1..20] |> should equal 232792560
+        Euclid.LeastCommonMultiple [|1L..20L|] |> should equal 232792560
--- f#/ProjectEuler/Euclid.fs
+++ f#/ProjectEuler/Euclid.fs
@@ -5,44 +5,39 @@ module Euclid =
     open NUnit.Framework
     open FsUnit
     open FsCheck
-
-    let gcd a b = 
-        let rec gcd' x y = if y = 0 then abs x else gcd' y (x % y) 
-        gcd' (abs a) (abs b)
-
-    let lcm x y = x * (y / (gcd x y))
-
-    [<Test>]
-    let ``lcm 9 10`` () = lcm 9 10 |> should equal 90
+    open MathNet.Numerics
     
-    [<Test>]
-    let ``lcm 5 12 6`` () = Seq.reduce lcm [5; 12; 6] |> should equal 60
+
+    //let inline gcd (a:^T) (b:^T) : ^T = Euclid.GreatestCommonDivisor a b
+    //let inline gcd a b = Euclid.GreatestCommonDivisor a b
+    //    let rec gcd' x y = if y = 0 then abs x else gcd' y (x % y) 
+    //    gcd' (abs a) (abs b)
+
+    //type Gcd = Gcd with
+    //    static member inline gcd( a:int64, b:int64) = Euclid.GreatestCommonDivisor a b
+    //    static member inline gcd( a:bigint, b:bigint) = Euclid.GreatestCommonDivisor a b
     
-    [<Test>]
-    let ``lcm 5 10 20`` () = Seq.reduce lcm [5; 10; 20] |> should equal 20
-    
-    [<Test>]
-    let ``lcm 1..15`` () = Seq.reduce lcm [1..15] |> should equal 360360
-    
-    [<Test>]
-    let ``lcm 1..17`` () = Seq.reduce lcm [1..17] |> should equal 12252240
-    
-    [<Test>]
-    let ``lcm 1..19`` () = Seq.reduce lcm [1..19] |> should equal 232792560
-    
-    [<Test>]
-    let ``lcm 1..21`` () = Seq.reduce lcm [1..21] |> should equal 232792560
+    //let inline gcd(a:^a, b:^a) = ((^a) : (static member gcd : ^a) (a, b))
 
+    //let inline lcm x y = x * (y / (Euclid.GreatestCommonDivisor x y))
 
-    [<Test>]
-    let ``gcd a a = a`` () =
-        let ``gcd a a = a'`` (a:int) = gcd a a = abs a
-        Check.QuickThrowOnFailure ``gcd a a = a'``
-
-    [<Test>]
-    let ``every common divisor also divides their gcd`` () =
-        let ``gcd divisibility`` n m p = gcd (p*n) (p*m) = abs p * gcd n m
-        Check.QuickThrowOnFailure ``gcd divisibility``
-
-
-
+   // [<Test>]
+   // let ``lcm 9 10`` () = lcm 9 10 |> should equal 90
+   // 
+   // [<Test>]
+   // let ``lcm 5 12 6`` () = Seq.reduce lcm [5; 12; 6] |> should equal 60
+   // 
+   // [<Test>]
+   // let ``lcm 5 10 20`` () = Seq.reduce lcm [5; 10; 20] |> should equal 20
+   // 
+   // [<Test>]
+   // let ``lcm 1..15`` () = Seq.reduce lcm [1..15] |> should equal 360360
+   // 
+   // [<Test>]
+   // let ``lcm 1..17`` () = Seq.reduce lcm [1..17] |> should equal 12252240
+   // 
+   // [<Test>]
+   // let ``lcm 1..19`` () = Seq.reduce lcm [1..19] |> should equal 232792560
+   // 
+   // [<Test>]
+   // let ``lcm 1..21`` () = Seq.reduce lcm [1..21] |> should equal 232792560
