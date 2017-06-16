b20c301d1dfed1a185916980f428e782dd7db890
Jeremy Wright <jeremy@quiescent.us>
2017-06-15 22:36:45 -0700

    Authoring more cucumber files.

--- kotlin/problems/src/test/kotlin/Steps.kt
+++ kotlin/problems/src/test/kotlin/Steps.kt
@@ -1,10 +1,14 @@
 import cucumber.api.java8.En
 import com.codestrokes.multiple3Or5
+import cucumber.api.DataTable
+import cucumber.api.Delimiter
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 
 class Steps : En {
     private var result: Int? = null
+
+    private var compositeNumber: Int? = null
 
     init {
 
@@ -13,6 +17,22 @@ class Steps : En {
 
         Given("^I sum of even-valued terms below (\\d+)$",
                 { upperBound: Int -> throw cucumber.api.PendingException() })
+
+        Given("^the composite number (\\d+)$",
+                { compositeNumber: Int -> this.compositeNumber = compositeNumber })
+
+       // Then("^the prime factors are (\\d+(?:, \\d+)*)$", //How can we get a List<Int>
+       // Then("^the prime factors are (\\d+), (\\d+), (\\d+), (\\d+)$",
+        Then("^the prime factors are$",
+                { factors: DataTable ->
+
+                    for (row in factors.raw()) {
+                        print(row[0])
+                    }
+                })
+
+        Then("^the largest prime factor is (\\d+)$",
+                { largestPrime: Int -> throw cucumber.api.PendingException() })
 
         Then("^the result is (\\d+).$",
                 { a: Int -> assertThat(result, `is`(a));})
---
+++ kotlin/problems/src/test/resources/features/Problem-003.feature
@@ -0,0 +1,16 @@
+Feature: Problem 3
+
+  # The prime factors of 13195 are 5, 7, 13 and 29.
+  # What is the largest prime factor of the number 600851475143 ?
+
+  Scenario: Largest prime factor example
+    Given the composite number 12195
+    Then the prime factors are
+    | 5 |
+    | 7 |
+    | 13|
+    |29 |
+
+ Scenario: SLargest prime factor
+    Given the composite number 600851475143
+    Then the largest prime factor is 6857
